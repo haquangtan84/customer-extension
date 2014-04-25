6e0402663877f00820e8cb9c80be8b4453922def
haquangtan <haquangtan84@gmail.com>
2014-04-25 14:08:24 +0700

    Remove target folder

--- customer-extension-webapp/target/classes/org/exoplatform/portlet/customer/templates/index_.groovy
+++
@@ -1,8 +0,0 @@
-package org.exoplatform.portlet.customer.templates;
-;out.print(org.exoplatform.portlet.customer.templates.Cindex.s0);
-
-public static class Cindex
-{
-public static final juzu.io.CharArray$Simple s0 = new juzu.io.CharArray$Simple('QUAN KY KHACH HANG');
-public static final Map<Integer, juzu.impl.template.spi.juzu.dialect.gtmpl.Foo> TABLE = [:];
-}
--- customer-extension-webapp/target/generated-sources/annotations/org/exoplatform/portlet/customer/CustomerPortlet.java
+++
@@ -1,9 +0,0 @@
-package org.exoplatform.portlet.customer;
-import javax.annotation.Generated;
-@Generated(value={})
-public class CustomerPortlet extends juzu.bridge.portlet.JuzuPortlet {
-@Override
-protected String getApplicationName(javax.portlet.PortletConfig config) {
-return "org.exoplatform.portlet.customer";
-}
-}
